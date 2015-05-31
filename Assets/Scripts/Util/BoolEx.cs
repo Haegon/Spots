c86285cb39cfaf03c1c826290a33a14c2c870310
gohn0929 <gohn@ncrewcorp.com>
2015-05-31 18:47:53 +0900

    boolex

---
+++ Assets/Scripts/Util/BoolEx.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class BoolEx
+{
+	public static int ToInt(this bool b)
+	{
+		if ( b ) return 1;
+		else return 0;
+	}
+	
+	public static bool ToBool(this int i)
+	{
+		if ( i > 0 ) return true;
+		else return false;
+	}
+}
